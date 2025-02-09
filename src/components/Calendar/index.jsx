bf6e2c7297525440794b60f4e5455ef1d4d04a58
Taehyeungkim <kth1837@snu.ac.kr>
2025-02-09 23:20:54 +0900

    hw: make Calendar Page

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import CheckListIcon from "../../assets/checklist.png";
 import DayModal from "../DayModal";
 
@@ -12,9 +13,84 @@ import {
   isSameMonth,
   isSameDay,
 } from "date-fns";
+import { useState } from "react";
 
 const Calendar = () => {
-  return <></>;
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const startDate = startOfWeek(startOfMonth(currentDate));
+  const endDate = endOfWeek(endOfMonth(currentDate));
+
+  const days = [];
+  let day = startDate;
+
+  while (day <= endDate) {
+    days.push(day);
+    day = add(day, { days: 1 });
+  }
+
+  const handlePrevMonth = () => setCurrentDate(sub(currentDate, { months: 1 }));
+  const handleNextMonth = () => setCurrentDate(add(currentDate, { months: 1 }));
+
+  const handleDayClick = (date) => {
+    setSelectedDate(date);
+  };
+
+  const closeModal = () => {
+    setSelectedDate(null);
+  };
+  return (
+    <>
+      <h1 className="text-start m-12 mb-8 px-1">History</h1>
+      <div className="p-4 mx-8">
+        <div className="flex justify-between items-center mb-8">
+          <button className="text-xl" onClick={handlePrevMonth}>
+            &lt;
+          </button>
+          <h2 className="text-lg font-semibold">
+            {format(currentDate, "MMMM yyyy")}
+          </h2>
+          <button className="text-xl" onClick={handleNextMonth}>
+            &gt;
+          </button>
+        </div>
+        <div className="grid grid-cols-7 gap-6 text-center">
+          {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((day) => (
+            <div key={day} className="font-bold">
+              {day}
+            </div>
+          ))}
+          {days.map((date, index) => (
+            <div
+              key={index}
+              className={`p-2 rounded-full cursor-pointer ${
+                isSameMonth(date, currentDate)
+                  ? isSameDay(date, new Date())
+                    ? "bg-[#20c997] text-white"
+                    : "hover:bg-gray-200"
+                  : "text-gray-400"
+              }`}
+              onClick={() => handleDayClick(date)}
+            >
+              {format(date, "d")}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {selectedDate && (
+        <DayModal selectedDate={selectedDate} closeModal={closeModal} />
+      )}
+
+      <Link to="/">
+        <img
+          src={CheckListIcon}
+          className="absolute w-15 right-8 bottom-10 opacity-20 hover:opacity-100 transition-opacity duration-120"
+        />
+      </Link>
+    </>
+  );
 };
 
 export default Calendar;
